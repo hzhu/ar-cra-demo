2c208b6377aa1f7d88a35e018c29023314ff4732
hzhu <hz@henryzhu.me>
2022-01-16 15:11:41 -0800

    Add example of interacting with Arweave

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Arweave from "arweave";
+export const arweave = Arweave.init({
+  host: "arweave.net",
+  port: 443,
+  protocol: "https"
+});
 
 function App() {
+  useEffect(() => {
+    const txId = "PNZ8rrBmuxmtpdcqIh9N8DFWV3xpiScT2lYldn_euaI";
+
+    const run = async () => {
+      // Interact with Arweave
+      try {
+        const data = await arweave.transactions.getData(txId, {
+          decode: true,
+          string: true
+        });
+
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    run();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
